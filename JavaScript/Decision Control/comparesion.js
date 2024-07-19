1bfc9089265e6981f1dbc88a8da1f166ab4b6a99
csubham2370 <112157533+subhogithub1234@users.noreply.github.com>
2024-07-19 09:38:58 +0530

    commit

---
+++ JavaScript/Decision Control/comparesion.js
@@ -0,0 +1,27 @@
+// console.log(5==5)
+// console.log(5===5)
+// console.log(5===6)
+// console.log(5!==6)
+
+let age = 18;
+console.log(age > 18);
+
+age = 78;
+
+console.log(age > 18);
+age = 18;
+console.log(age >= 18);
+
+age =56;
+
+console.log(age > 47 + 34);
+
+age = 40;
+console.log('Program Start')
+if ( age >= 18){
+  console.log("You are an adult.");
+}
+else{
+  console.log('You are a minor.')
+}
+console.log('Program end.')
