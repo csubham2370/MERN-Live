56cc843539c8c5f3ccf34665498993563c39ceb2
csubham2370 <144363196+csubham2370@users.noreply.github.com>
2024-07-25 00:25:04 +0530

    commit

---
+++ JavaScript/Class 25 funcation/funcation.js
@@ -0,0 +1,60 @@
+function fun(){
+
+  console.log('hello world!!!');
+}
+
+fun()
+
+function fun2(){
+  let sum = 10;
+  let sum2 =10;
+
+  return sum + sum2;
+}
+
+console.log(fun2());
+
+
+function fun3(p1, p2){
+
+  let mul = p1 * p2;
+  return mul;
+}
+
+let res = fun3(5,6);
+console.log(res);
+
+
+function fun4(){
+
+  num = 12 + 14;
+  console.log(num);
+}
+
+fun4()
+
+
+function fun5(){
+
+  console.log(yoy);
+  var yoy = 'ABCD';
+  console.log(yoy);
+}
+fun5()
+
+// function fun6(){
+
+//   console.log(yoy);
+//   let yoy = 'ABCD';
+//   console.log(yoy);
+// }
+// fun6()
+
+// function fun7(){
+
+//   // console.log(yoy);
+//   const yoy = 'XYZ';
+//   yoy = 'dfr'
+//   console.log(yoy);
+// }
+// fun7()
