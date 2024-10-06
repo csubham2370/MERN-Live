164aaebc888d6f2749c02e33da02c2b97d24b527
csubham2370 <csubham2370@gmail.com>
2024-10-05 22:25:46 -0700

    done

---
+++ React Moderate/to-do-app-using-useState-use-ref/to-do-app-using-useState-use-ref/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './components/Header';
+import InputText from './components/InputText';
+
+const App = () => {
+  return (
+    <>
+
+    <Header />
+    <InputText />
+    
+    
+    </>
+    
+  )
+}
+
+export default App;
---
+++ React Moderate/to-do-app-using-useState-use-ref/to-do-app-using-useState-use-ref/src/components/InputText.jsx
@@ -0,0 +1,11 @@
+
+
+const InputText = () =>{
+  <>
+<input type="text"  className="bg-slate-400"/>
+  </>
+
+
+}
+
+export default InputText;
